753af8713e00ccbab4c49719f8b2ec7f1e0a2932
Anna Nalimova <avnalimova@mail.ru>
2023-02-28 00:17:48 +0300

    36

---
+++ 36/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив заполненный случайными числами.
+// Найтите сумму элементов стоящих на нечетных индексах.
+int[] GetRandomArray( int size, int minValue, int maxValue)
+{
+    int[]arr = new int[size];
+    for (int i=0;i<size; i++)
+    {
+        arr[i]=new Random().Next(minValue,maxValue);
+    }     
+    return arr;  
+}
+
+int GetSumOddEl(int[] mass)
+{
+    int sum =0;
+    int count= mass.Length;
+   for(int i=0;i<count;i++)
+   {
+     if(i%2!=0) sum= sum+mass[i];
+   }
+   return sum;
+}
+
+int[] massive = GetRandomArray(5,1,10);
+int sum = GetSumOddEl(massive);
+Console.WriteLine($"[{String.Join(", " ,massive)} ]-> {sum}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
