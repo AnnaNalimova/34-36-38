ee3b21498645921fda6417db37cc74fd05d9b51d
Anna Nalimova <avnalimova@mail.ru>
2023-02-28 00:43:48 +0300

    38

---
+++ 38/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив целых чисел.Найдите разницу 
+//между максимальным и минимальным элементом массива.
+int[] GetRandomArray( int size, int minValue, int maxValue)
+{
+    int[]arr = new int[size];
+    for (int i=0;i<size; i++)
+    {
+        arr[i]=new Random().Next(minValue,maxValue);
+    }     
+    return arr;  
+}
+int FindDiff(int[] mass)
+{
+    int max=mass[0];
+    int min=mass[0];
+    int diff=0;
+    for(int i=0;i<mass.Length;i++)
+    {
+        if(mass[i]>max) max=mass[i];
+        if(mass[i]< min)  min=mass[i];
+        diff=max-min;
+    }
+    return diff;
+}
+int[] massive=GetRandomArray(7,3,10);
+int diff=FindDiff(massive);
+Console.WriteLine($"[{String.Join(", " ,massive)} ]-> {diff}");
+
+
