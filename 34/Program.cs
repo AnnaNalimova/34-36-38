0106829d819df1744ddeb60d938a7fff67f621d3
Anna Nalimova <avnalimova@mail.ru>
2023-02-28 00:17:05 +0300

    34

---
+++ 34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив заполненный случайными положительными  трехзначными числами.
+//Напишите программу , которая покажет колличество четных чисел в массиве.
+int[] GetRandomArray( int size, int minValue, int maxValue)
+{
+    int[]arr = new int[size];
+    for (int i=0;i<size; i++)
+    {
+        arr[i]=new Random().Next(minValue,maxValue);
+    }     
+    return arr;  
+}
+int FindEvenNambers(int[] array)
+{ 
+    int count=0;
+    for(int i=0;i<array.Length;i++)
+    {
+        if(array[i]%2==0)
+        count++;
+    }
+    return count;
+}
+int[]massive=GetRandomArray(7,100,1000);
+int count=FindEvenNambers(massive);
+Console.WriteLine($"[{String.Join(", " ,massive)} ]->{count}");
